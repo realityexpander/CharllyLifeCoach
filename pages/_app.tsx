84047c9f395174992c0a2998a3acd8c80a746e76
Chris Athanas <realityexpander@gmail.com>
2023-03-09 17:34:01 -0600

    Add image, basic screen, css, bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/globals.css'  // Global styles - overrides bootstrap
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
