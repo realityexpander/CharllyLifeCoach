173da290b6be4551ee02b0a5502f7a8cc2bfa9f9
Chris Athanas <realityexpander@gmail.com>
2023-03-09 22:14:09 -0600

    Add actual endpoint processing

---
+++ pages/api/chatgpt.ts
@@ -0,0 +1,79 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
+
+declare global {
+  interface String {
+    trimIndent(): string
+  }
+}
+
+String.prototype.trimIndent = function () {
+  // Remove leading whitespace from each line
+  // and remove empty lines
+  const lines = this.split('\n')
+  const trimmedLines = lines.map((line) => line.trim())
+  const filteredLines = trimmedLines.filter((line) => !isBlankString(line))
+
+  return filteredLines.join('\n')
+}
+
+const config = new Configuration(
+  {
+    apiKey: process.env.OPENAPI_API_KEY,
+    //basePath: 'https://api.openai.com',
+  }
+)
+
+const openai = new OpenAIApi(config)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log(req.query)
+  const prompt = req.query.prompt as string
+
+  if(prompt.length==0 || !prompt) return res.status(400).json({error: 'No prompt provided'})
+  if(prompt.length > 100) return res.status(400).json({error: 'Prompt too long'})
+
+  const promptContext = `
+     Create a motivational quote that gives the opposite of good advice in the style of Chris Farley on the following Topic:
+      Topic: ${prompt}
+
+      DO NOT SIGN YOUR NAME TO THE QUOTE
+
+      Motivational Quote:
+  `.trimIndent()
+
+  const request = {
+    model: "text-davinci-003",
+    prompt: promptContext,
+    max_tokens: 500,
+    temperature: 0.9,
+    // top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
+    // stop: ['\n', ' Human:', ' AI:'],
+  }
+
+  try {
+    const completion = await openai.createCompletion(request)
+    const response = completion.data.choices[0].text
+    res.status(200).json({ response })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: 'Something went wrong'})
+  }
+}
+
+// Check if string is empty or only contains whitespace
+function isBlankString(str): boolean {
+  if (str.length === 0) return true
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') return false
+  }
+
+  return true
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,58 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form, Spinner } from 'react-bootstrap'
+import { FormEvent, useState } from 'react'
 
-import mainImage from '@/assets/images/CraigFurleyMotivationalSpeaker1.png'
-import { FormEvent } from 'react'
+import charllyTopImage from '@/assets/images/CraigFurleyMotivationalSpeaker1.png'
+import charllyGoForIt from '@/assets/images/CharllyGoForIt.png'
 
 export default function Home() {
 
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    console.log("handleSubmit")
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const prompt = formData.get("prompt")?.toString().trim();
   
     if (prompt) {
       try {
-        // setQuote("");
-        // setQuoteLoadingError(false);
-        // setQuoteLoading(true);
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
   
-        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
+        const response = await fetch("/api/chatgpt?prompt=" + encodeURIComponent(prompt));
         const body = await response.json();
-        console.log(body)
-        // setQuote(body.quote);
+        setQuote(body.response);
       } catch (error) {
-        console.error(error);
-        // setQuoteLoadingError(true);
+        setQuoteLoadingError(true);
       } finally {
-        // setQuoteLoading(false);
+        setQuoteLoading(false);
       }
     }
   }
-  
 
   return (
     <>
       <Head>
-        <title>Life Motivation AI Coach Charlly Finely</title>
+        <title>Charlly the Finely: The Finest Life Motivation AI Coach</title>
         <meta name="description" content="Charlly Finely motivates YOU!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Charlly Finely</h1>
-        <h5>AI Motivational Life Coach</h5>
+        <h1>Charlly the Finely</h1>
+        <h5>Finest AI Motivational Life Coach</h5>
         <p>"Help <i>me</i> help <i>YOU!</i></p>
-        <br/>
+        {/* <br/> */}
 
         {/* <img src="../../assets/images/CraigFurleyMotivationalSpeaker1.png" alt="Craig Furley" height="200"/> */}
         <div className={styles.mainImageContainer}>
           <Image 
-            src={ mainImage } 
+            src={ charllyTopImage } 
             // width={ 200 } 
             fill
             alt={ "Craig Furley"}
@@ -59,28 +60,50 @@ export default function Home() {
             className={styles.mainImage}
           />
         </div>
-        <p>Powered by FinelySuccessAI™</p>
-        <br/>
+        {/* <p>Powered by FinelySuccessAI™</p> */}
+        {/* <br/> */}
 
-        <div>Enter a topic and Charlly will generate a SUPER motivational quote</div>
+        {/* <h5>Enter a topic and Charlly will generate a SUPER MOTIVATIONAL suggestion</h5> */}
+        {/* <p>For'ie, Fohre!</p> */}
+
+        <br/>
+        <br/>
 
         <Form onSubmit={handleSubmit} className={styles.inputForm}>
           <Form.Group className='mb-3' controlId='prompt-input'>
-            <Form.Label>Create a cringy quote about...</Form.Label>
+            <Form.Label>Charlly, give me the Finelyest advice about...</Form.Label>
             <Form.Control
               name='prompt'
-              placeholder='e.g. success, fear, potatoes'
+              placeholder='e.g. money, love, health'
               maxLength={100}
             />
           </Form.Group>
           {/* <Button type='submit' className='mb-3' disabled={quoteLoading}> */}
           <Button type='submit' className='mb-3' >
-            Motivate Me Charlly!
+            Help Me Charlly!
           </Button>
         </Form>
-        {/* {quoteLoading && <Spinner animation='border' />}
+
+        {quoteLoading && <Spinner animation='border' />}
         {quoteLoadingError && "Something went wrong. Please try again."}
-        {quote && <h5>{quote}</h5>} */}
+        {quote && 
+          <>
+              <div className="d-flex flex-row justify-content-start">
+              <div className="p-2">
+                <Image
+                  src = { charllyGoForIt }
+                  alt = { "Charlly the Finely"}
+                  height = { 150 }
+                  className={styles.goForItImage}
+                />
+              </div>
+              <div className="p-2">
+                <h5>Charlly says:</h5>
+                <p> { quote } </p>
+              </div>
+            </div>
+          </>
+        }
 
       </main>
     </>
